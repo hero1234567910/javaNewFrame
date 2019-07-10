894dd9cc9cc428a0596c21617c578404c9b9142a
wzlggg <wzl1888@126.com>
2019-07-10 17:01:44 +0800

    用户登陆

--- src/entry/index/index.js
+++ src/entry/index/index.js
@@ -1,19 +1,14 @@
 import Vue from 'vue'
-import IndexView from './index.vue'
+import ElementUI from 'element-ui'
+import indexView from './index.vue'
 import axios from 'axios'
-import $ from 'jquery'
-import  FastClick  from  'fastclick'
-import router from './router'
-import '../../../static/css/jquery-weui.min.css'
-import '../../../static/lib/weui.css'
-import '../../../static/css/local.css'
-   
-   FastClick.attach(document.body);
-//Vue.use(ElementUI);
-Vue.prototype.axios = axios
+import router from './router/index.js'
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+Vue.prototype.axios = axios;
 new Vue({
   el: '#app',
   router,
-  render: h => h(IndexView)
-});
-
+  render: h => h(indexView)
+})
--- src/entry/index/router/index.js
+++ src/entry/index/router/index.js
@@ -1,22 +1,39 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import userBinding from '../components/userBinding.vue'
-import home from '../components/home.vue'
-import mycenter from '../components/mycenter.vue'
+import Router from 'vue-router';
+import Login from '../components/views/Login.vue';
+import Home from '../components/views/home.vue';
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
-  routes: [
-  	{
-      path: '/',
-      name: 'mycenter',
-      component: mycenter
-   },
-    {
-      path: '/userBinding',
-      name: 'userBinding',
-      component: userBinding
-    }
-  ]
+    routes:[
+        {
+            path:'/Login',
+            name:'Login',
+            component:Login
+        },
+        {
+            path:'/',
+            name:'Home',
+            component:Home
+        },
+    ]
 })
+
+// router.beforeEach((to, from, next) => {
+//     // to and from are both route objects. must call `next`.
+//     let user = localStorage.getItem('m_token');
+//     if(to.path == '/Login'){
+//         if(user){
+//             next({path:'/'})
+//         }else{
+//             next();
+//         }
+//     }else{
+//         if(!user){
+//             next({path:'/login'})
+//         }else{
+//             next();
+//         }
+//     }
+// });
